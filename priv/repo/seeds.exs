83426823b8f85c9733a83d9f5ea913217d071bfd
Tim Mecklem <timothy@mecklem.com>
2022-01-20 23:38:17 -0500

    Add import of database files to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+require Logger
+
+parts_file = Path.join(File.cwd!(), "priv/csv_files/parts.csv")
+{:ok, number_of_inserts} = BrickSeeker.Parts.PartImporter.import_from_file(parts_file)
+Logger.info("Imported #{number_of_inserts} parts from CSV")
